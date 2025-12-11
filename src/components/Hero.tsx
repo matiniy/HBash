3d4923122413cb6381ff274491dd5215495304e1
matiniy <metamatn@gmail.com>
2025-12-11 21:21:13 +0400

    Add 3 new hero images to carousel: 1_photo_01_dte.jpg, 13_photo_12.jpg, 54_photo_56.jpg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,13 +8,14 @@ import HeroCarousel from './HeroCarousel';
 // Hero images array - Add your image filenames here
 // Simply add new images to /public/images/hero/ and add their filenames to this array
 const heroImages = [
+  '/images/hero/1_photo_01_dte.jpg',
   '/images/hero/2_photo_2.jpg',
+  '/images/hero/13_photo_12.jpg',
   '/images/hero/20_photo_22.jpg',
   '/images/hero/21_photo_23.jpg',
+  '/images/hero/54_photo_56.jpg',
   '/images/hero/59_photo_59.jpg',
   '/images/hero/62A73F24-0FF0-45FB-8099-165430CA0911.JPG',
-  // Add more images here as you upload them to /public/images/hero/
-  // Example: '/images/hero/your-new-image.jpg',
 ];
 
 const Hero: React.FC = () => {
